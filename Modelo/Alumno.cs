20580ce21f4dc185a1e69002c297d287373373d9
adrian17marroquin <adrian17marroquin@gmail.com>
2026-04-10 00:59:24 -0600

    Agregar archivos de proyecto.

---
+++ Modelo/Alumno.cs
@@ -0,0 +1,8 @@
+﻿public class Alumno
+{
+    public int Id { get; set; }
+    public string Carnet { get; set; }
+    public string NombreCompleto { get; set; }
+    public decimal Latitud { get; set; }
+    public decimal Longitud { get; set; }
+}
